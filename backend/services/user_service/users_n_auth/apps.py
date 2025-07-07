99ddf53f1fcf21132b71b1f4ca03d7c9675d88bf
Nii Commey <jude.niicommey@outlook.com>
2025-07-07 10:21:53 +0000

    Revert "Initial Commit"

--- backend/services/user_service/users_n_auth/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersNAuthConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users_n_auth'
-
-    def ready(self):
-        import users_n_auth.signals
--- backend/services/user_service/users_n_auth/throttles.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.throttling import SimpleRateThrottle, UserRateThrottle
-
-class PasswordResetThrottle(SimpleRateThrottle):
-    scope = 'password_reset'
-
-    def get_cache_key(self, request, view):
-        return self.get_ident(request)
