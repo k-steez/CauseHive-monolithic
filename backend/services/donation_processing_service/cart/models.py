21acdc2a285a13d3b9b5fbc3a15aedbcea49acf9
Nii Commey <jude.niicommey@outlook.com>
2025-07-09 16:19:01 +0000

    Fixed the models for the cart app

--- backend/services/donation_processing_service/cart/models.py
+++ backend/services/donation_processing_service/cart/models.py
@@ -2,6 +2,9 @@ import uuid
 
 from django.db import models
 
+from donations.models import Donation
+
+
 # Create your models here.
 class Cart(models.Model):
     CART_STATUS_CHOICES = [
@@ -10,13 +13,17 @@ class Cart(models.Model):
         ('abandoned', 'Abandoned'),
     ]
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user_id = models.UUIDField(db_index=True, default=uuid.uuid4, editable=False)
+    user_id = models.UUIDField(db_index=True, default=uuid.uuid4, editable=False, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=20, choices=CART_STATUS_CHOICES, default='active')
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    event_or_cause_id = models.UUIDField(db_index=True, default=uuid.uuid4, editable=False) # References either an event or cause ID from the event_causes_services
+    cause_id = models.UUIDField(db_index=True, default=uuid.uuid4, editable=False) # References either an event or cause ID from the event_causes_services
+    donation = models.OneToOneField(Donation, on_delete=models.SET_NULL, null=True, blank=True, related_name='cart_item')
     donation_amount = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('cart', 'cause_id')
